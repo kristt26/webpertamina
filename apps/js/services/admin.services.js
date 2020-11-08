5dde297fccf4a02c6070348131901ee532fb1a62
kristt26 <kristt26@gmail.com>
2020-11-08 20:18:36 +0900

    fix

--- apps/js/services/admin.services.js
+++ apps/js/services/admin.services.js
@@ -5,13 +5,13 @@ angular.module('admin.service', [])
     .factory('PersetujuanKimServices', PersetujuanKimServices)
     ;
 
-function DaftarUserServices($http, $q, StorageService, $state, helperServices, AuthService) {
+function DaftarUserServices($http, $q, StorageService, $state, helperServices, AuthService, message) {
     var controller = helperServices.url + 'users';
     var service = {};
     service.data = [];
     service.instance = false;
     return {
-        get: get
+        get: get, post: post, put: put
     };
 
     function get() {
@@ -35,6 +35,50 @@ function DaftarUserServices($http, $q, StorageService, $state, helperServices, A
                 }
             );
         }
+        return def.promise;
+    }
+    function post(param) {
+        var def = $q.defer();
+        $http({
+            method: 'post',
+            url: helperServices.url + 'administrator/createuser/' + param.roles,
+            data: param,
+            headers: AuthService.getHeader()
+        }).then(
+            (res) => {
+                service.data.push(res.data);
+                def.resolve(res.data);
+            },
+            (err) => {
+                def.reject(err);
+                message.error(err);
+            }
+        );
+        return def.promise;
+    }
+    function put(param) {
+        var def = $q.defer();
+        $http({
+            method: 'put',
+            url: helperServices.url + 'administrator/updateuser/' + param.id,
+            data: param,
+            headers: AuthService.getHeader()
+        }).then(
+            (res) => {
+                var data = service.data.find(x => x.id == param.id);
+                if (data) {
+                    data.firstName = param.firstName;
+                    data.lastName = param.lastName;
+                    data.userName = param.userName;
+                    data.email = param.email;
+                }
+                def.resolve(res.data);
+            },
+            (err) => {
+                def.reject(err);
+                message.error(err);
+            }
+        );
         return def.promise;
     }
 
--- apps/js/services/helper.services.js
+++ apps/js/services/helper.services.js
@@ -5,9 +5,13 @@ angular.module("helper.service", [])
 
 function helperServices() {
     var service = {};
+    // service.url = "http://192.168.1.5/api/";
+    // service.base = "http://192.168.1.5/";
     service.url = "http://36.94.6.214/api/";
     service.base = "http://36.94.6.214/";
     service.jenisPengajuan = ['Baru', 'Perpanjangan'];
+    service.roles = ['Company', 'Approval1', 'HSE', 'Manager', 'Gate'];
 
-    return { url: service.url, base: service.base, jenisPengajuan: service.jenisPengajuan };
+
+    return { url: service.url, base: service.base, jenisPengajuan: service.jenisPengajuan, roles: service.roles };
 }
