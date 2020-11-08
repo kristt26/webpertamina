2623773168f7771a46e395a5ef75900beaa517de
kristt26 <kristt26@gmail.com>
2020-11-08 20:43:40 +0900

    sd

--- apps/js/controllers/admin.controllers.js
+++ apps/js/controllers/admin.controllers.js
@@ -1,21 +1,12 @@
 angular
     .module('admin.controller', [])
-    .controller('adminController', adminController)
     .controller('adminDaftarUserController', adminDaftarUserController)
     .controller('adminlistpemeriksaanController', adminlistpemeriksaanController)
     .controller('adminpersetujuankimController', adminpersetujuankimController)
     .controller('adminkim', adminkim)
     .controller('adminhistoritrukController', adminhistoritrukController);
 
-function adminController($scope, $state, AuthService) {
-    if (!AuthService.userIsLogin()) {
-        $state.go("login");
 
-    } else {
-
-    }
-
-}
 
 function adminDaftarUserController($scope, DaftarUserServices, helperServices, message) {
     $scope.roles = helperServices.roles;
