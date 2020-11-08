ce600bb1add65784e9fd524224a9db3bf291303e
kristt26 <kristt26@gmail.com>
2020-11-08 22:45:35 +0900

    ok

--- apps/js/services/helper.services.js
+++ apps/js/services/helper.services.js
@@ -5,10 +5,10 @@ angular.module("helper.service", [])
 
 function helperServices() {
     var service = {};
-    // service.url = "http://192.168.1.5/api/";
-    // service.base = "http://192.168.1.5/";
-    service.url = "http://36.94.6.214/api/";
-    service.base = "http://36.94.6.214/";
+    service.url = "http://192.168.1.5/api/";
+    service.base = "http://192.168.1.5/";
+    // service.url = "http://36.94.6.214/api/";
+    // service.base = "http://36.94.6.214/";
     service.jenisPengajuan = ['Baru', 'Perpanjangan'];
     service.roles = ['Company', 'Approval1', 'HSE', 'Manager', 'Gate'];
 
